3ef9a2c2223edf5cdc71d586ba83dc3032019f8b
James Donnelly <donnellyfj@gmail.com>
2022-11-29 12:58:30 -0500

    Test gradient

--- whackamole.py
+++ whackamole.py
@@ -20,6 +20,7 @@ round = 0
 correct = 0
 wrong = 0
 
+# Fuction of gradient effect
 def color_gradient(LED_stick, r1, b1, g1, r2, g2, b2, LED_length):
     # Subtract 1 from LED_length because there is one less transition color
     # than length of LEDs
@@ -60,72 +61,6 @@ def run_gradient():
     b2 = random.randint(0, 255)
 
     color_gradient(my_stick, r1, g1, b1, r2, g2, b2, 10)
-
-# Fuction of rainbow effect
-def walking_rainbow(LED_stick, rainbow_length, LED_length, delay, value):
-    red_array = [None] * LED_length
-    blue_array = [None] * LED_length
-    green_array = [None] * LED_length
-
-    global round,correct,wrong
-
-    for j in range(0, rainbow_length):
-
-        for i in range(0, LED_length):
-            # There are n colors generated for the rainbow
-            # The value of n determins which color is generated at each pixel
-            n = i + 1 - j
-
-            # Loop n so that it is always between 1 and rainbow_length
-            if n <= 0:
-                n = n + rainbow_length
-
-            # The nth color is between red and yellow
-            if n <= math.floor(rainbow_length / 6):
-                red_array[i] = 255
-                green_array[i] = int(math.floor(6 * 255 / rainbow_length * n))
-                blue_array[i] = 0
-            
-            # The nth color is between yellow and green
-            elif n <= math.floor(rainbow_length / 3):
-                red_array[i] = int(math.floor(510 - 6 * 255 / rainbow_length * n))
-                green_array[i] = 255
-                blue_array[i] = 0
-            
-            # The nth color is between green and cyan
-            elif n <= math.floor(rainbow_length / 2):
-                red_array[i] = 0
-                green_array[i] = 255
-                blue_array[i] = int(math.floor(6 * 255 / rainbow_length * n - 510))
-            
-            # The nth color is between blue and magenta
-            elif n <= math.floor(5 * rainbow_length / 6):
-                red_array[i] = int(math.floor(6 * 255 / rainbow_length * n - 1020))
-                green_array[i] = 0
-                blue_array[i] = 255
-            
-            # The nth color is between magenta and red
-            else:
-                red_array[i] = 255
-                green_array[i] = 0
-                blue_array[i] = int(math.floor(1530 - (6 *255 / rainbow_length * n)))
-
-        # Set all the LEDs to the color values accordig to the arrays
-        LED_stick.set_all_LED_unique_color(red_array, green_array, blue_array, LED_length)
-
-        if value:  # just button A pressed Making correct
-            round += 1
-            correct += 2
-            print(correct)
-            time.sleep(0.1)
-
-        elif not value:  # just button B pressed Making wrong
-            round += 1
-            wrong += 1
-            print(wrong)
-            time.sleep(0.1)
-
-        time.sleep(delay)
 
 def run_whack():
     # Configure Buttons
@@ -267,7 +202,7 @@ def run_whack():
                     #my_stick.LED_off()
                     
                 # client.publish(topic, val)
-                if round < 2:
+                if round > 2:
                     time.sleep(0.01)
                     run_gradient()
                     time.sleep(0.01)
